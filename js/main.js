11cb7a346584d749c4932ed20446a1d81d2d4fe9
Timur Gaziiev <trilochan.sbbbm@gmail.com>
2015-07-12 16:01:12 +0300

    first fix

--- js/main.js
+++ js/main.js
@@ -36,6 +36,33 @@ $(document).ready(function ($) {
 
 
 	};
+	//responsive code begin
+        //you can remove responsive code if you don't want the slider scales
+        //while window resizes
+        function ScaleSlider() {
+            var parentWidth = $('#slider1_container').parent().width();
+            if (parentWidth) {
+//				alert(parentWidth);
+				if (parentWidth <= 380) {
+					jssor_slider1.$ScaleWidth(100 + '%');
+					
+				} else {
+				
+					jssor_slider1.$ScaleWidth(parentWidth);
+				}
+            }
+            else
+                window.setTimeout(ScaleSlider, 30);
+        }
+        //Scale slider after document ready
+        ScaleSlider();
+                                        
+        //Scale slider while window load/resize/orientationchange.
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
+        //responsive code end
+//	slider2 inicialization
 	var jssor_slider2 = new $JssorSlider$('carousel', optionss);
 	
 //	TABS
